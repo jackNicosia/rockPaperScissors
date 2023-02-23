a11c1089dc29a105eb0763363008f00f5fd3ff14
Jack <jackrileynicosia@yahoo.com>
2023-02-23 16:25:06 -0500

    got the scores to reset

--- game.js
+++ game.js
@@ -15,9 +15,10 @@
   let playerScore = 0;
   let playerSelection;
   let computerSelection;
-  const choices = ["Rock","Paper","Scissors"];
+  const choices = ["rock","paper","scissors"];
   const buttons = document.querySelectorAll('.btn');
   const playerPoints = document.querySelector('#playerScore');
+  const computerPoints = document.querySelector('#computerScore');
 
   buttons.forEach((button)=>{button.addEventListener('click',()=>{
   
@@ -35,21 +36,20 @@
 
   function playRound() {
     let result;
-    computerSelection = getComputerChoice()
-    if ((playerSelection == "rock" && computerSelection == "Rock") || 
-        (playerSelection == "paper" && computerSelection == "Paper") ||
-        (playerSelection == "scissors" && computerSelection == "Scissors")) {     
+    computerSelection = getComputerChoice();
+     if ((playerSelection == "rock" && computerSelection == "rock") || 
+        (playerSelection == "paper" && computerSelection == "paper") ||
+        (playerSelection == "scissors" && computerSelection == "scissors")) {     
      result = "Its a tie!"; 
-    } else if ((playerSelection == "rock" && computerSelection == "Scissors") ||
-              (playerSelection == "scissors" && computerSelection == "Paper") ||
-              (playerSelection == "paper" && computerSelection == "Rock")) {
+    } else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+              (playerSelection == "scissors" && computerSelection == "paper") ||
+              (playerSelection == "paper" && computerSelection == "rock")) {
       result = "You Win!"; 
-      playerPoints.textContent = ++playerScore
-      playerScore++;
+      playerPoints.textContent = ++playerScore;
       
     } else {
       result = "You lose!";
-      computerScore++;
+      computerPoints.textContent = ++computerScore;
     }
     console.log(result);
     }
@@ -60,18 +60,91 @@
     function game(){
       let finalScore;
       playRound();
+    
+      
   
       if (computerScore == 5){
        finalScore = "Computer wins the final duel!"
+       computerScore = 0;
+       playerScore = 0;
+       computerPoints.textContent = computerScore;
+       playerPoints.textContent = playerScore;
       } else if (playerScore == 5){
-        finalScore = "You have defeated the evil computer and saved your family!"
+        finalScore = "Game over, you win!"
+        computerScore = 0;
+        playerScore = 0;
+        computerPoints.textContent = computerScore;
+        playerPoints.textContent = playerScore;
       }
       console.log(finalScore);
     }
       
+/*
+  function endGame(){
+  if (finalScore = "Game over, you win!")
+  computerScore == 0;
+  }
+  */
 
+  /* Use the for loop to output even numbers from 2 to 10.
+    for (let i = 2; i < 11; i += 2){
+    alert(i);
+  }
+ 
+ Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+
+  let i =0;
+  while (i < 3) {
+  alert(`number ${i}!` );
+  i++;
+  }
+
+
+
+  Write a loop which prompts for a number greater than 100. 
+  If the visitor enters another number – ask them to input again.
+
+The loop must ask for a number until either the visitor enters a 
+number greater than 100 or cancels the input/enters an empty line.
+
+Here we can assume that the visitor only inputs numbers. There’s 
+no need to implement a special handling for a non-numeric input in this task.
   
-   
-  
+
+let num; 
+ 
+do { 
+  num = prompt("Enter a number greater than 100", 0);
+} while (num <=100 && num);
 
 
+
+num = prompt("Enter a number");
+let n = 2;
+num % n === 0 ? : n++
+
+
+
+
+for (let n=2; num % n <= 0; n++){
+  alert(num % n)
+}
+*/
+/*
+
+let num = prompt("Enter a number");
+
+nextPrime: 
+for (let n = 2; n <= num; n++){       //for each n if n is less or equal to num, add 1 to n
+  for(let j = 2; j < n; j++) {     //incrementing  the divisor
+   if (n % j == 0) continue nextPrime; //if not prime, skip the remaining part of the body and continue to next iteration of n
+  }
+
+  alert (n); // alert primes
+}
+
+*/
